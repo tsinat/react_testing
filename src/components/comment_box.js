27b7292c4121b518fe3113527a13ef2e482f8f0a
Tsinat Zeree <cheru87@gmail.com>
2016-10-18 23:50:29 -0700

    end of chai testing

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -14,13 +14,14 @@ class CommentBox extends Component {
     }
     handleSubmit(e){
         e.preventDefault();
-        
+
         this.props.saveComment(this.state.comment);
         this.setState({comment: ''});
     }
     render() {
         return (
             <form  onSubmit={this.handleSubmit.bind(this) } className='comment-box'>
+                <h4>Add a Comment</h4>
                 <textarea
                     value={ this.state.comment }
                     onChange={this.handleChange.bind(this)}/>
