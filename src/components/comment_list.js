fff59e77a85d6f0394ea0c2f94fd481289d03c39
Tsinat Zeree <cheru87@gmail.com>
2016-10-19 23:32:28 -0700

    unit test refactor

--- src/components/comment_list.js
+++ src/components/comment_list.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import  { connect } from 'react-redux';
 
 const CommentList = (props) => {
+
     const list = props.comments.map(comment => <li key={comment} >{comment}</li>);
+
     return (
         <ul className='comment-list'>
             { list }
